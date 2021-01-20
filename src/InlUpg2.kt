793b63526d846d606bcc18d31cd3b4bd8e12acb9
kdhedman <kenth.david.hedman@gmail.com>
2021-01-20 19:34:30 +0100

    Initial commit

---
+++ src/InlUpg2.kt
@@ -0,0 +1,39 @@
+import java.io.File
+
+fun main() {
+    val input = File("src/input.txt").readLines()
+    val tomteMap = hashMapOf<String, List<String>>()
+
+    input.map {
+        for((index, value) in it.split(" ").withIndex()){
+            if(index > 0)
+                tomteMap.put(
+                    it.split(" ")[0],
+                    it.split(" ").filterIndexed({index, _ -> index>0 }).toList()
+                )
+        }
+    }
+    while(true){
+        println("Skriv namnet på en tomte för att se underordnade.")
+        val input = readLine()!!
+        if(input == "0") break
+        else println(listSubordinates(tomteMap, input).joinToString(separator = ", "))
+    }
+}
+
+fun listSubordinates(hashMap: Map<String, List<String>>,entry: String) : MutableList<String> {
+    if (hashMap.containsKey(entry)) {
+        val result = mutableListOf<String>()
+        for(tomte in hashMap[entry] ?: error("")){
+            result += tomte
+            result += listSubordinates(hashMap, tomte)
+        }
+        return result
+    } else {
+        return mutableListOf()
+    }
+}
+
+class Tomte(private val name: String) {
+    override fun toString(): String = name
+}
